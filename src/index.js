50263cc78bfb630672adfd22ade4cdd52893a9a2
huntdavid175 <huntdavid175@gmail.com>
2020-09-18 09:50:58 +0000

    completed with menu activestate

--- src/index.js
+++ src/index.js
@@ -3,24 +3,37 @@ import { displayMenuPage } from "./menu";
 import { displayContactPage } from "./contact";
 
 const parentDiv = document.getElementById("content");
+const allMenuBtn= document.querySelectorAll(".nav-link")
 const homeBtn = document.getElementById("home");
 const menuBtn = document.getElementById("menu");
 const contactBtn = document.getElementById("contact");
 
 window.addEventListener("load", displayHomepage);
 
-homeBtn.addEventListener("click", function () {
+homeBtn.addEventListener("click", function (e) {
+	removeActiveBtn();
+	e.target.classList.add('active');
 	parentDiv.innerHTML = "";
 	displayHomepage();
 });
 
-menuBtn.addEventListener("click", function () {
+menuBtn.addEventListener("click", function (e) {
+	removeActiveBtn();
+	e.target.classList.add('active');
 	parentDiv.innerHTML = "";
 
 	displayMenuPage();
 });
 
-contactBtn.addEventListener("click", function () {
+contactBtn.addEventListener("click", function (e) {
+	removeActiveBtn();
+	e.target.classList.add('active');
 	parentDiv.innerHTML = "";
 	displayContactPage();
 });
+
+function removeActiveBtn() {
+	allMenuBtn.forEach(function(btn) {
+		btn.classList.remove('active')
+	})
+}
